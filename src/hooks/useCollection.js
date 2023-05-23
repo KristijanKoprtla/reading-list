b74c4bc692dda416033f3eddc93a74ef930ef6fc
BombMarley <kile146@gmail.com>
2023-05-23 22:24:56 +0200

    Add Firestore query to fetch users books

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,16 +1,23 @@
 // real-time collection data
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { db } from "../firebase/config";
 
 //firebase imports
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 
-export const useCollection = (c) => {
+export const useCollection = (c, _q) => {
     const [documents, setDocuments] = useState(null)
+
+    // set up query
+    const q = useRef(_q).current
 
     useEffect(() => {
         let ref = collection(db, c)
+
+        if (q) {
+            ref = query(ref, where(...q))
+        }
     
         const unsub = onSnapshot(ref, (snapshot) => {
             let results = []
@@ -21,7 +28,7 @@ export const useCollection = (c) => {
         })
 
         return () => unsub()
-      }, [c])
+      }, [c, q])
 
     return { documents }
 }
